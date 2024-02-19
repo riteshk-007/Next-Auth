6289f871ff7d3bf23d5ed7cced871750645a96bf
riteshk-007 <codeshorts007@gmail.com>
2024-02-19 14:48:52 +0530

    input validation

--- app/(auth)/user/page.js
+++ app/(auth)/user/page.js
@@ -2,11 +2,44 @@
 import Button from "@/components/Button";
 import Input from "@/components/Input";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const User = () => {
   const [variate, setVariate] = useState("LOGIN");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const toggleVariant = () => {
+    if (variate === "LOGIN") {
+      setVariate("REGISTER");
+    } else {
+      setVariate("LOGIN");
+    }
+  };
+
+  const onSubmit = (data) => {
+    if (variate === "LOGIN") {
+      console.log("Login", data);
+    }
+    if (variate === "REGISTER") {
+      console.log("Register", data);
+    }
+  };
+
   return (
-    <form className="w-full flex justify-center items-center h-screen space-y-5 flex-col">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-full flex justify-center items-center h-screen space-y-5 flex-col"
+    >
       <h1
         className="text-4xl font-semibold text-black"
         style={{ fontFamily: "Poppins" }}
@@ -19,6 +52,8 @@ const User = () => {
           id="name"
           type="text"
           placeholder="Enter your name"
+          errors={errors}
+          register={register}
         />
       )}
       <Input
@@ -26,34 +61,33 @@ const User = () => {
         id="email"
         type="email"
         placeholder="Enter your email"
+        errors={errors}
+        register={register}
+        required={true}
       />
+      {errors.email && (
+        <p className="text-rose-500 text-sm">Email is required.</p>
+      )}
       <Input
         label="Password"
         id="password"
         type="password"
         placeholder="Enter your password"
+        errors={errors}
+        register={register}
+        required={true}
       />
-      {variate === "LOGIN" ? (
-        <span className="text-sm  text-black  flex gap-2">
-          {" Don't have an account? "}{" "}
-          <p
-            className="cursor-pointer font-semibold"
-            onClick={() => setVariate("REGISTER")}
-          >
-            Register
-          </p>
-        </span>
-      ) : (
-        <span className="text-sm  text-black  flex gap-2">
-          Already have an account?
-          <p
-            className="cursor-pointer font-semibold"
-            onClick={() => setVariate("LOGIN")}
-          >
-            Login
-          </p>
-        </span>
+      {errors.password && (
+        <p className="text-rose-500 text-sm">Password is required.</p>
       )}
+      <div>
+        {variate === "LOGIN"
+          ? "Don't have an account?"
+          : "Already have an account?"}
+      </div>
+      <div onClick={toggleVariant} className="underline cursor-pointer">
+        {variate === "LOGIN" ? "Create an account" : "Login"}
+      </div>
       <Button type="submit" variate={variate} />
     </form>
   );
--- components/Input.js
+++ components/Input.js
@@ -1,4 +1,15 @@
-const Input = ({ label, id, type, placeholder, ref }) => {
+"use client";
+
+import clsx from "clsx";
+const Input = ({
+  label,
+  id,
+  type,
+  placeholder,
+  register,
+  errors,
+  required,
+}) => {
   return (
     <div className="w-full md:w-1/3">
       <label
@@ -8,11 +19,17 @@ const Input = ({ label, id, type, placeholder, ref }) => {
         {label}
       </label>
       <input
-        className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-        type={type}
-        placeholder={placeholder}
         id={id}
-      ></input>
+        type={type}
+        autoComplete={id}
+        placeholder={placeholder}
+        {...register(id, { required })}
+        className={clsx(
+          `flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none
+           focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50`,
+          errors[id] && "focus:ring-rose-500 "
+        )}
+      />
     </div>
   );
 };
